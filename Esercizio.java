d4f5321d2b676a0a47ce3d274d799f75be2c543d
ChristianBarile3ID <christianalessandro.barile@marconibari.edu.it>
2025-11-17 18:50:37 +0000

    Consegna

--- Esercizio.java
+++ Esercizio.java
@@ -13,13 +13,29 @@ class Esercizio {
     public static record Max(int massimo, int rIdx, int cIdx) { }
 
     // Metodo per calcolare il massimo nella matrice
-    public static Max calcolaMassimo( /*scrivere qui i parametri richiesti dall'esercizio*/ ) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
+    public static Max calcolaMassimo(int[][]M, int nR, int nC) {
+        int max, idxR, idxC;
+        idxR = 0;
+        idxC = 0;
+        for (int i = 0; i<nR; ++i) {
+            for (int j = 0; j<nC; ++j) {
+                if (M[i][j]>M[idxR][idxC]) {
+                    idxR = i;
+                    idxC = j;
+                }
+            }
+        }
+        max = M[idxR][idxC];
+        return new Max(max, idxR, idxC);
     }
 
     // Metodo per calcolare la media di una colonna
-    public static float calcolaMedia( /*scrivere qui i parametri richiesti dall'esercizio*/ ) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO        
+    public static float calcolaMedia(int[][]M, int nR, int colonna) {
+        float somma = 0;
+        for (int i = 0; i<nR; ++i) {
+            somma += M[i][colonna];
+        }
+        return somma/nR;       
     }
 
     // Metodo per riempire la matrice con valori casuali
@@ -28,13 +44,30 @@ class Esercizio {
         Random rand = new Random();
         for (int i=0; i <= RIGHE-1; i=i+1 ) {
             for (int j=0; j <= COLONNE-1; j=j+1) {
-                M[i][j] = valMin + rand.nextInt((valMax+1)-valMin);
+                M[i][j] = rand.nextInt((valMax+1)-valMin) + valMin;
             }
         }
     }
 
     public static void main(String args[]) {
-        //SCRIVERE QUI IL CODICE RICHIESTO DALL'ESERCIZIO
+        int n = 7;
+        int m = 5;
+        int [][] M = new int[n][m];
+        riempiCasuale(M, n, m, 298, 314);
+        System.out.println("Temperature registrate: \n");
+        UtilsMatrice.visualizza(M);
+        Max max = calcolaMassimo(M, n, m);
+        System.out.println("\nTemperatura massima: " + max.massimo());
+        System.out.println("Verificatasi il giorno: " + (max.rIdx()+1));
+        System.out.println("Alle ore: " + (max.cIdx()+11));
+        float [] medieColonne = new float[m];
+        for (int j = 0; j<m; ++j) {
+            medieColonne[j] = calcolaMedia(M, n, j);
+        }
+        System.out.println();
+        for (int i = 0; i<m; ++i) {
+            System.out.println("Media fascia oraria " + (i+11) + "-" + (i+12) + ": " + medieColonne[i]);
+        }
     }
 }
 
